da1e8ac79f9e0fe8fdca4456eb20a1da9126c0c3
hjkhalid <hk810773@gmail.com>
2023-03-04 17:01:22 +0530

    Did minor refactorting in method & variable name, add method & class level comment

--- src/Main.java
+++ src/Main.java
@@ -1,64 +1,86 @@
 
 import java.util.*;
+
+/**
+ * This calculates the average of values which are not the power of two.
+ * @Author Khalid Hussain
+ */
 public class Main {
-    public static void main(String[] args) {
-
-        //creating the object of Scanner class
+    public static void main(String[] args) throws Exception {
+//        creating the object of Scanner class
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of Array");
-        int sizeOfArray=sc.nextInt();
-        int [] arr= new int[sizeOfArray];
+        System.out.println("Enter the size of Array :");
+        int sizeOfArray = sc.nextInt();
+        int [] arr = new int[sizeOfArray];
 
-      // taking the input in array
-        for(int i=0;i<sizeOfArray;i++){
+//        taking the input in array
+        for(int i = 0; i < sizeOfArray; i++) {
             arr[i]= sc.nextInt();
         }
-      // storing  size  of arraylist non-multiple two numbers  in a variable with the help of calling the function nonPowerOfTwoNumbers
-        int sizeOfNonPowerTwo=nonPowerOfTwoNumbers(arr).size();
 
+        /**
+         *
+         * Storing size  of arraylist non-multiple two numbers  in a variable
+         * with the help of calling the
+         * function nonPowerOfTwoNumbers
+         */
+        int modifiedArrSize = buildArray(arr).size();
+//      output of non-multiple of 2 numbers
+        System.out.println("Non-power of 2 numbers:" + buildArray(arr));
 
-      //output of non-multiple of 2 numbers
-
-        System.out.println("Non-multiple of 2 numbers:"+nonPowerOfTwoNumbers(arr));
-
-        // output of the average of non-multiple of 2 numbers
-
-        System.out.println( "The Average of non-multiple of 2 Numbers:"+sumOfNonPowerOfTwo(nonPowerOfTwoNumbers(arr))/sizeOfNonPowerTwo);
-    }
-// function to find the element of input arr which is non-multiple of 2
-    private static  boolean isPowerOfTwo(int num) {
-      boolean a=false;
-
-      if( num>1 && (int)(Math.ceil((Math.log(num) / Math.log(2))))
-
-                == (int)(Math.floor(((Math.log(num) / Math.log(2)))))){
-          a=true;
-      }
-      else{
-          a=false;
-      }
-        return a;
+//      output of the average of non-multiple of 2 numbers
+        System.out.println( "The Average of non-Power of 2 Numbers:" + sumOfModifiedArr(buildArray(arr)) / modifiedArrSize);
     }
 
-    // In this function storing non-multiple of 2 in a arraylist
-    public static ArrayList nonPowerOfTwoNumbers(int [] nonTwoPower ){
-        ArrayList<Integer> nums=new ArrayList<>();
-        for(int i=0;i<nonTwoPower.length;i++) {
-            if(isPowerOfTwo(nonTwoPower[i])==false){
+    /**
+     * This method returns boolean based on whether the num is powerOfTwo
+     * @param num
+     * @return
+     * @throws Exception
+     */
+    private static boolean isPowerOfTwo(int num) throws Exception {
+        boolean temp = false;
+        try
+        {
+            float val  = (float) (Math.log(num) / Math.log(2));
+            if(num>1 && ( (int) (Math.ceil(val)) == (int) (Math.floor((val))) ) ) {
+                temp = true;
+            }
+        }
+        catch (Exception e)
+        {
+           throw new Exception("Cannot calculate log(0) ", e);
+        }
+        return temp;
+    }
+
+    /**
+     * The method will store the numbers which are not
+     * the power the 2.
+     * @param nonTwoPower
+     * @return
+     */
+    private static ArrayList buildArray(int [] nonTwoPower ) throws Exception {
+        ArrayList<Integer> nums = new ArrayList<>();
+
+        for(int i = 0; i < nonTwoPower.length; i++) {
+            if(!isPowerOfTwo(nonTwoPower[i])) {
                 nums.add(nonTwoPower[i]);
             }
         }
         return nums;
     }
 
-    // Calculating the sum of non-multiple of 2 numbers
-    public static int sumOfNonPowerOfTwo(ArrayList<Integer> nums){
-        int sum =0;
-        for (int i=0;i<nums.size();i++){
+    /**
+     * To add the modified Array which contains values excluding num^2.
+     * @param nums
+     * @return
+     */
+    private static int sumOfModifiedArr(ArrayList<Integer> nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++){
             sum += nums.get(i);
         }
         return sum;
     }
-
-
 }
